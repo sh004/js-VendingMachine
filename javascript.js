8fa7577fa7b2f75d0edb09c3961cc6008acccfbd
yamamoto <yamamoto@s14.yeskist.net>
2016-01-20 11:08:56 +0900

    購入した商品名を配列化　ドラッグアンドドロップ実装中

--- javascript.js
+++ javascript.js
@@ -1,10 +1,9 @@
 /*　メモ
 	・お金の投入ができない
-	・お金の位置
 	・ボタンが光らない
-	・商品を一度に複数購入できない	←配列で解決？
 */
 
+var a = 0;
 var pay = 0;									//支払い金額合計
 var num = document.getElementById("num");		//金額表示：金額
 var en = document.getElementById("en");			//金額表示：円マーク
@@ -13,11 +12,12 @@ var one = document.getElementsByClassName("one");		//ボタン：110円
 var three = document.getElementsByClassName("three");	//ボタン：130円
 var six = document.getElementsByClassName("six");		//ボタン：160円
 
-var itemName = "";		//商品名
+var purchaseNum = 0;
+var itemName = [];		//商品名
 var itemPrice = 0;		//商品の値段
 
-var change = 0;			//おつり総計
-var paper, coin;		//おつり紙幣, おつり硬貨
+var change = 0;							//おつり総計
+var paper = 0, coin = 0;						//おつり紙幣, おつり硬貨
 var getPaper = new Boolean(false);		//判定：紙幣返却用
 var getCoin = new Boolean(false);		//判定：硬貨返却用
 
@@ -45,7 +45,6 @@ function handleDrop(e) {
 	}
 	return false;
 }
-
 function handleDragEnd(e) {
 	this.style.opacity = '1';
 	[].forEach.call(cols, function (col) {
@@ -61,6 +60,15 @@ var cols = document.querySelectorAll('#wallet .money');
 	col.addEventListener('drop', handleDrop, false);
 	col.addEventListener('dragend', handleDragEnd, false);
 });
+/* a = pay + 入れたお金;
+if (a > 4200) {
+	document.text.backlog.value += "上限に達しました。\n";
+	goBottom();
+	change += 入れたお金;	//そのままおつりになる
+} else {
+	pay += 入れたお金;
+	num.innerText = pay;
+} */
 
 /*--------------- ここから仮 ---------------*/
 //金額表示
@@ -85,52 +93,65 @@ if (pay >= 110 && pay < 130) {
 //商品ボタン押下
 function buttonEnter(btnNo) {
 	switch(btnNo) {
-		case 1: itemName = "コカ･コーラ";
+		case 1: itemName[purchaseNum] = "コカ･コーラ";
 				itemPrice = 160;
+				purchaseNum++;
 				break;
-		case 2: itemName = "アクエリアス";
+		case 2: itemName[purchaseNum] = "アクエリアス";
 				itemPrice = 160;
+				purchaseNum++;
 				break;
-		case 3: itemName = "い･ろ･は･す";
+		case 3: itemName[purchaseNum] = "い･ろ･は･す";
 				itemPrice = 110;
+				purchaseNum++;
 				break;
-		case 4: itemName = "スプライト";
+		case 4: itemName[purchaseNum] = "スプライト";
 				itemPrice = 160;
+				purchaseNum++;
 				break;
-		case 5: itemName = "ファンタ グレープ";
+		case 5: itemName[purchaseNum] = "ファンタ グレープ";
 				itemPrice = 160;
+				purchaseNum++;
 				break;
-		case 6: itemName = "コーヒー";
+		case 6: itemName[purchaseNum] = "コーヒー";
 				itemPrice = 130;
+				purchaseNum++;
 				break;
-		case 7: itemName = "カフェラテ";
+		case 7: itemName[purchaseNum] = "カフェラテ";
 				itemPrice = 130;
+				purchaseNum++;
 				break;
-		case 8: itemName = "爽健美茶";
+		case 8: itemName[purchaseNum] = "爽健美茶";
 				itemPrice = 130;
+				purchaseNum++;
 				break;
-		case 9: itemName = "コーンポタージュ";
+		case 9: itemName[purchaseNum] = "コーンポタージュ";
 				itemPrice = 130;
+				purchaseNum++;
 				break;
-		case 10: itemName = "おしるこ";
+		case 10: itemName[purchaseNum] = "おしるこ";
 				 itemPrice = 130;
+				 purchaseNum++;
 				 break;
-		case 11: itemName = "ホットレモン";
+		case 11: itemName[purchaseNum] = "ホットレモン";
 				 itemPrice = 130;
+				 purchaseNum++;
 				 break;
-		case 12: itemName = "贅沢ミルクココア";
+		case 12: itemName[purchaseNum] = "贅沢ミルクココア";
 				 itemPrice = 130;
+				 purchaseNum++;
 				 break;
-		case 13: itemName = "紅茶花伝";
+		case 13: itemName[purchaseNum] = "紅茶花伝";
 				 itemPrice = 130;
+				 purchaseNum++;
 				 break;
-		default: itemName ="";
 	}
 	
 	if (pay < itemPrice) {
 		document.text.backlog.value += itemPrice - pay + "円足りません。\n";
 		goBottom();
-		itemName ="";
+		purchaseNum--;
+		itemName[purchaseNum] = "";
 	} else {
 		pay = pay - itemPrice;
 		num.innerText = pay;
@@ -138,13 +159,26 @@ function buttonEnter(btnNo) {
 }
 //商品取り出し
 function outputItem() {
-	if(itemName == ""){
+	var all = "";
+	if(itemName.length == 0){
 		document.text.backlog.value += "商品を購入してください。";
 		goBottom();
 	} else {
-		document.text.backlog.value += itemName + "を購入しました。";
+		for (var i = 0; i <= purchaseNum-1; i++) {
+			if (i == 0) {
+				all += itemName[i];
+			} else {
+				all += ", " + itemName[i];
+			}
+		}
+		if (purchaseNum == 1) {
+			document.text.backlog.value += all + "を購入しました。";
+		} else {
+			document.text.backlog.value += all + "\n以上の" + purchaseNum + "本を購入しました。";
+		}
 		goBottom();
-		itemName = "";
+		itemName = [];
+		purchaseNum = 0;
 	}
 	document.text.backlog.value += "\n";
 	goBottom();
@@ -153,17 +187,19 @@ function outputItem() {
 //----- お金返却処理 -----//
 //レバーを引く
 function hasPulled() {
-	change = pay;
+	change += pay;
 	pay = 0000;
 	num.style.color = "#1c1c1c";
 	en.style.color = "#1c1c1c";
 	
 	//紙幣のおつり
-	paper = Math.floor(change / 1000) * 1000;
+	paper += Math.floor(change / 1000) * 1000;
 	getPaper = new Boolean(true);
 	
 	//硬貨のおつり
-	coin =+ (change + '').slice(-3);
+	var b = 0;
+	b =+ (change + '').slice(-3);
+	coin += (b - 0);
 	getCoin = new Boolean(true);
 	
 	document.text.backlog.value += "返却レバーを引きました。\n";
@@ -176,6 +212,7 @@ function returnPaper() {
 			document.text.backlog.value += "おつりはありません。";
 		} else {
 			document.text.backlog.value += paper + "円が返却されました。";
+			paper = 0;
 		}
 		getPaper = new Boolean(false);
 	} else {
@@ -191,6 +228,7 @@ function returnCoin() {
 			document.text.backlog.value += "おつりはありません。";
 		} else {
 			document.text.backlog.value += coin + "円が返却されました。";
+			coin = 0;
 		}
 		getCoin = new Boolean(false);
 	} else {
